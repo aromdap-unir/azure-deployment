95dd6a402ae0b34af32c27d693616dbe091f4a7e
Adrian Romero-Dapena <adrianromerodapena@riseup.net>
2021-02-15 19:54:33 +0100

    Terraform basics

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.1"
+    }
+  }
+}
+
+# crea un service principal y rellena los siguientes datos para autenticar
+provider "azurerm" {
+features {}
+  subscription_id = "7e748d57-c321-4373-a637-de2de9efcae4"
+  client_id       = "4c5d624e-c48f-427e-891d-b86428f7e1fb"
+  client_secret   = "DqA9RpxWD9stA8muTGLWX_5B1.Xpp8bkrt"
+  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+
+resource "azurerm_resource_group" "rg" {
+    name     =  "kubernetes_rg"
+    location = var.location
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Storage account
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
+
+resource "azurerm_storage_account" "stAccount" {
+    name                     = "staccountcp2" 
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
